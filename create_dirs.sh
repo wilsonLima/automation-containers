7735b5cff172484a76608d7c85a5d45feb296ba5
Wilson Lima <wilson.slima@gmail.com>
2019-12-14 14:58:03 -0300

    Correção criação de diretórios

--- create_dirs.sh
+++ create_dirs.sh
@@ -2,6 +2,5 @@
 
 mkdir -p jenkins/jenkins_home
 mkdir -p rundeck/rundeck_data
-mkdir -p postgresql/data
-mkdir -p postgresql/data_pgadmin4
-chmod 777 -R postgresql/data_pgadmin4
+mkdir -p postgresql/postgresql
+mkdir -p postgresql/postgresql_data
